99bc56ff79cdf1e72f776d83a8773e8901290380
André Silveira <andre.silveira1silva@gmail.com>
2024-11-20 15:08:18 -0300

    chore: add queries of Members of the Organization.

---
+++ apps/api/src/http/routes/members/get-members.ts
@@ -0,0 +1,91 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import * as zod from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getMembers(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/members',
+      {
+        schema: {
+          tags: ['members'],
+          summary: 'Get all organization members',
+          security: [{ bearerAuth: [] }],
+          params: zod.object({
+            slug: zod.string(),
+          }),
+          response: {
+            200: zod.object({
+              members: zod.array(
+                zod.object({
+                  id: zod.string().uuid(),
+                  userId: zod.string().uuid(),
+                  role: roleSchema,
+                  name: zod.string().nullable(),
+                  email: zod.string().email(),
+                  avatarUrl: zod.string().url().nullable(),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'User')) {
+          throw new UnauthorizedError(
+            `You're not allowed to see organization members.`,
+          )
+        }
+
+        const members = await prisma.member.findMany({
+          select: {
+            id: true,
+            role: true,
+            user: {
+              select: {
+                id: true,
+                name: true,
+                email: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            organizationId: organization.id,
+          },
+          orderBy: {
+            role: 'asc',
+          },
+        })
+
+        const membersWithRoles = members.map(
+          ({ user: { id: userId, ...user }, ...member }) => {
+            return {
+              userId,
+              ...user,
+              ...member,
+            }
+          },
+        )
+
+        return reply.send({ members: membersWithRoles })
+      },
+    )
+}
