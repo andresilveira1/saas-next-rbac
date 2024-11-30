f5fb2696365a03e5dd8d1abe884d63b91b3c3eed
André Silveira <andre.silveira1silva@gmail.com>
2024-11-29 23:18:53 -0300

    chore: create sign in with Github and Email actions.

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,54 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import * as zod from 'zod'
+
+import { signInWithEmail } from '@/http/sign-in-with-email'
+
+const signInSchema = zod.object({
+  email: zod
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address.' }),
+  password: zod.string().min(1, { message: 'Please, provide your password.' }),
+})
+
+export async function signInWithEmailAndPassword(data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithEmail({
+      email,
+      password,
+    })
+    const cookieStore = await cookies()
+
+    cookieStore.set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    })
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  redirect('/')
+}
