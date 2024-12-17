29346e5607d7a5ad93ab3eb976508f1ae2f76abd
André Silveira <andre.silveira1silva@gmail.com>
2024-12-16 21:28:26 -0300

    feat: add page invite members.

---
+++ apps/web/src/app/invite/[id]/page.tsx
@@ -0,0 +1,134 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { CheckCircle, LayoutDashboard, LogIn, LogOut } from 'lucide-react'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+
+import { auth, isAuthenticated } from '@/auth/auth'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { acceptInvite } from '@/http/accept-invite'
+import { getInvite } from '@/http/get-invite'
+
+interface InviteProps {
+  params: Promise<{
+    id: string
+  }>
+}
+
+dayjs.extend(relativeTime)
+
+export default async function Invite({ params }: InviteProps) {
+  const { id } = await params
+
+  const { invite } = await getInvite({ inviteId: id })
+  const isUserAuthenticated = Boolean(await isAuthenticated())
+
+  let currentUserEmail = null
+
+  if (isUserAuthenticated) {
+    const { user } = await auth()
+
+    currentUserEmail = user.email
+  }
+
+  const userIsAuthenticatedWithSameEmailFromInvite =
+    currentUserEmail === invite.email
+
+  async function signInFromInvite() {
+    'use server'
+
+    const cookieStore = await cookies()
+    cookieStore.set('inviteId', id)
+
+    redirect(`/auth/sign-in?email=${invite.email}`)
+  }
+
+  async function acceptInviteAction() {
+    'use server'
+
+    await acceptInvite({ inviteId: id })
+
+    redirect('/')
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center px-4">
+      <div className="flex w-full max-w-sm flex-col justify-center space-y-6">
+        <div className="flex flex-col items-center space-y-4">
+          <Avatar className="size-16">
+            {invite.author?.avatarUrl && (
+              <AvatarImage src={invite.author.avatarUrl} />
+            )}
+            <AvatarFallback />
+          </Avatar>
+
+          <p className="text-balance text-center leading-relaxed text-muted-foreground">
+            <span className="font-medium text-foreground">
+              {invite.author?.name ?? 'Someone'}
+            </span>{' '}
+            invited you to join{' '}
+            <span className="font-medium text-foreground">
+              {invite.organization.name}
+            </span>
+            .{' '}
+            <span className="text-xs">{dayjs(invite.createdAt).fromNow()}</span>
+          </p>
+        </div>
+
+        <Separator />
+
+        {!isUserAuthenticated && (
+          <form action={signInFromInvite}>
+            <Button type="submit" variant="secondary" className="w-full">
+              <LogIn className="mr-2 size-4" />
+              Sign in to accept the invite
+            </Button>
+          </form>
+        )}
+
+        {userIsAuthenticatedWithSameEmailFromInvite && (
+          <form action={acceptInviteAction}>
+            <Button type="submit" variant="secondary" className="w-full">
+              <CheckCircle className="mr-2 size-4" />
+              Join {invite.organization.name}
+            </Button>
+          </form>
+        )}
+
+        {isUserAuthenticated && !userIsAuthenticatedWithSameEmailFromInvite && (
+          <div className="space-y-4">
+            <p className="text-balance text-center leading-relaxed text-muted-foreground">
+              This invite was sent to{' '}
+              <span className="font-medium text-foreground">
+                {invite.email}
+              </span>{' '}
+              but you are currently authenticated as{' '}
+              <span className="font-medium text-foreground">
+                {currentUserEmail}
+              </span>
+            </p>
+
+            <div className="space-y-2">
+              <Button className="w-full" variant="secondary" asChild>
+                <a href="/api/auth/sign-out" className="cursor-pointer">
+                  <LogOut className="mr-2 size-4" />
+                  Sign out from {currentUserEmail}
+                </a>
+              </Button>
+
+              <Button className="w-full" variant="outline" asChild>
+                <Link href="/" className="cursor-pointer">
+                  <LayoutDashboard className="mr-2 size-4" />
+                  Back to dashboard
+                </Link>
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
